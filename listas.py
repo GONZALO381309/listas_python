9934cdcaef0a1ec6dccf8a5aa89ae6042bf99486
Gonzalo <gon.cortez@duocuc.cl>
2023-05-25 20:17:51 -0400

    Operaciones con listas

---
+++ listas.py
@@ -0,0 +1,26 @@
+#print("Hola Mundo")
+#lista=[1,"hola", 3.67,[1,2,3]]
+#print(lista)
+#print(lista[2])
+#print(lista[1])
+#print(lista[-1])
+#print(lista[-2])
+#print(lista[-3])
+#lista=[1,2]
+#lista.append(5)
+#print(lista)
+#lista=[1,2]
+#lista.extend([3,4])
+#print(lista)
+#lista=[1,3]
+#lista.insert(2,"casa")
+#print(lista)
+#lista=[1,2]
+#lista.pop(0)
+#print(lista)
+#lista_numerica=[11,3,7,8]
+#lista_numerica.sort()
+#print(lista_numerica)
+lista=[1,2,3,4]
+lista.remove(1)
+print(lista)
