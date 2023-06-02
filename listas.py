d061d7af3b83ffb5da6696d5a08ed4dbd6ff14d9
Gonzalo <gon.cortez@duocuc.cl>
2023-06-02 13:42:18 -0400

    Todo como comentario

--- listas.py
+++ listas.py
@@ -21,6 +21,41 @@
 #lista_numerica=[11,3,7,8]
 #lista_numerica.sort()
 #print(lista_numerica)
-lista=[1,2,3,4]
-lista.remove(1)
-print(lista)
+#lista=[1,2,3,4]
+#lista.remove(1)
+#print(lista)
+
+#ITEM 6-12 SISCO
+#X= 1 / 2 + 3 // 3 + 4 ** 2
+#print(X)
+
+#X=int (input("Ingrese valor 11: "))
+#y=int (input("Ingrese valor 4: "))
+#x= X % y
+#x= X % y
+#y= y % X
+#print(y)
+
+#x=int(input())
+#y=int(input())
+#x= x // y
+#y= y // x
+#print(1/1)
+
+#z = y = x = 1
+#print(x,y,z, sep="*")
+
+Y= 2 + 3 * 5. 
+print(Y)
+
+#x=1
+#y=2
+#z=x
+#x=y
+#y=z
+#print(x,y)
+
+#x=input()
+#y=input()
+
+#print(x+y)
