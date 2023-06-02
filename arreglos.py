aa3122e92a87599d09297d63368dcb4845879dee
Gonzalo <gon.cortez@duocuc.cl>
2023-06-01 20:23:51 -0400

    Operaciones y Random

--- arreglos.py
+++ arreglos.py
@@ -1,21 +1,40 @@
 import numpy as np
 #forma 1 desde lista
-lista=[1,2,3,4]
-arr= np.array(lista)
+#lista=[1,2,3,4]
+#arr= np.array(lista)
 #print(arr)
 
 #forma 2 directa
-arr2=np.array([5,6,7,8])
+#arr2=np.array([5,6,7,8])
 #print(arr2)
 
 
 #funciones con arreglo
-arreglo=np.array([5,6,7,8,9,10,11,12])
+#arreglo=np.array([5,6,7,8,9,10,11,12])
 #print(arreglo.ndim)
 #print(len(arreglo))
 #print(arreglo[4:7])
 #print(arreglo[0:7:2])
 #print(arreglo[::2])
-c=[i for i in range (0,101)]
-print(c[1:100:2]) #impares
-print(c[0:100:2])
+#c=[i for i in range (0,101)]
+#print(c[1:100:2]) #impares
+#print(c[0:100:2])
+
+arreglo=np.array ([4,8,15,16,23,42])
+contar_hasta=len(arreglo)
+for i in range (contar_hasta):
+    #imprime el contenido individual
+    print(arreglo[i])
+
+#imprimiendo el arreglo
+print(arreglo)
+
+#crear un acceso directo a un arreglo
+#arreglo_copia=arreglo[:]
+
+#copia real
+arreglo_copia=arreglo[:].copy()
+arreglo[0]=1000
+
+print("Original", arreglo)
+print("Copia", arreglo_copia)
