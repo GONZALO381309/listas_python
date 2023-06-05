baaa630ec98c2f01aa8d451f7cadf43ebe2eaf86
Gonzalo <gon.cortez@duocuc.cl>
2023-06-05 01:29:28 -0400

    modif

--- operacionesArreglos.py
+++ operacionesArreglos.py
@@ -1,13 +1,13 @@
 import numpy as np
-numeros_aleatorios=np.random.randint(0,100)
-print(numeros_aleatorios)
+#numeros_aleatorios=np.random.randint(0,100)
+#print(numeros_aleatorios)
 
-#x=range(0,10)
-#arreglo=np.array(x)
-#for n in x:
-#    arreglo[n]=np.random.randint(0,100)
-#print(arreglo)
-#print("El numero máximo: ",arreglo.max())
-#print("El numero minimo: ",arreglo.min())
-#print("El numero sumatoria: ",arreglo.sum())
-#print("El numero promedio: ",arreglo.mean())
+x=range(0,10)
+arreglo=np.array(x)
+for n in x:
+    arreglo[n]=np.random.randint(0,100)
+print(arreglo)
+print("El numero máximo: ",arreglo.max())
+print("El numero minimo: ",arreglo.min())
+print("El numero sumatoria: ",arreglo.sum())
+print("El numero promedio: ",arreglo.mean())
