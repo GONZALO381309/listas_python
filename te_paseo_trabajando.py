95367c055fa7211634373c719aa6f72ef04a2a26
Gonzalo <gon.cortez@duocuc.cl>
2023-07-12 21:05:28 -0400

    TRABAJANDO PRUEBA 3 TE_PASEO

--- te_paseo_trabajando.py
+++ te_paseo_trabajando.py
@@ -1,8 +1,10 @@
 import numpy as np
+import random
 menu_act=True
 reserva_act=True
 mascota_act=True
 buscar_activo=True
+seleccion_act=True
 fecha_reserva=""
 hora_de_paseo=""
 Lugar_de_inicio_paseo=""
@@ -11,7 +13,7 @@ mascotas_a_pasear=0
 nombre_persona_reserva=""
 num_reserva=0
 precio_de_la_paseada=0
-lista=[]
+
 while menu_act:
     print("-.-.-.TE PASEO.-.-.-.-")
     print("")
@@ -52,18 +54,19 @@ while menu_act:
         acumulador_precio=precio_de_la_paseada
         print("Precio ha quedado guardado Correctamente")
         print("")
-        lista=[acumulador_fecha,acumulador_hora,acumulador_inicio,acumulador_fin,acumulador_mascotas,acumulador_persona,acumulador_reserva,acumulador_precio]
+        lista=["acumulador_fecha","acumulador_hora","acumulador_inicio","acumulador_fin","acumulador_mascotas","acumulador_persona","acumulador_reserva","acumulador_precio"]
+        arreglo=np.array(lista)
+        for i in range(1):
+            print("La fecha de paseo es: ", acumulador_fecha)
+            print("La hora de paseo es: ", acumulador_hora)
+            print("El inicio del paseo comienza en: ", acumulador_inicio)
+            print("El termino del paseo finaliza en: ", acumulador_fin)
+            print("La cantidad de mascotas a pasear es/son: ", acumulador_mascotas)
+            print("La persona que agendo el servicio es Don/Señora: ", acumulador_persona)
+            print("El numero de la reserva es: ", acumulador_reserva)
+            print("El precio por el servicio de paseo es de: $ ", acumulador_precio)
         
-        print("La fecha de paseo es: ", acumulador_fecha)
-        print("La hora de paseo es: ", acumulador_hora)
-        print("El inicio del paseo comienza en: ", acumulador_inicio)
-        print("El termino del paseo finaliza en: ", acumulador_fin)
-        print("La cantidad de mascotas a pasear es/son: ", acumulador_mascotas)
-        print("La persona que agendo el servicio es Don/Señora: ", acumulador_persona)
-        print("El numero de la reserva es: ", acumulador_reserva)
-        print("El precio por el servicio de paseo es de: $ ", acumulador_precio)
-            
-        continue
+            continue
       
     if opcion == 2:
         print("Usted a seleccionado la opcion 2.- BUSCAR")
@@ -75,27 +78,132 @@ while menu_act:
                 else:
                     if op == 1:
                         nombre_persona_reserva=input("Ingrese el nombre de la persona a buscar: ")
-                        print("Verificando, por favor espere...........")
-                        
-                        if nombre_persona_reserva < 1:
-                            print("Debe ingresar al menos un caracter")
+                        print("")
+                        if nombre_persona_reserva == acumulador_persona:
+                            print("La fecha de paseo es: ", acumulador_fecha)
+                            print("La hora de paseo es: ", acumulador_hora)
+                            print("El inicio del paseo comienza en: ", acumulador_inicio)
+                            print("El termino del paseo finaliza en: ", acumulador_fin)
+                            print("La cantidad de mascotas a pasear es/son: ", acumulador_mascotas)
+                            print("La persona que agendo el servicio es Don/Señora: ", acumulador_persona)
+                            print("El numero de la reserva es: ", acumulador_reserva)
+                            print("El precio por el servicio de paseo es de: $ ", acumulador_precio)
+                            break
                         else:
+                            print("Verificando, por favor espere...........")
+                            print("No existe el nombre de la persona a buscar en la base de datos")
                             continue
                     if op == 2:
                         num_reserva=int(input("Ingrese numero de reserva: "))
-                        print("Verificando, por favor espere...........")
-                        
-                        if num_reserva != acumulador_reserva:
-                            print("No existe numero de reserva, favor verifique")
+                        print("")
+                        if num_reserva == acumulador_reserva:
+                            print("La fecha de paseo es: ", acumulador_fecha)
+                            print("La hora de paseo es: ", acumulador_hora)
+                            print("El inicio del paseo comienza en: ", acumulador_inicio)
+                            print("El termino del paseo finaliza en: ", acumulador_fin)
+                            print("La cantidad de mascotas a pasear es/son: ", acumulador_mascotas)
+                            print("La persona que agendo el servicio es Don/Señora: ", acumulador_persona)
+                            print("El numero de la reserva es: ", acumulador_reserva)
+                            print("El precio por el servicio de paseo es de: $ ", acumulador_precio)
+                            break
                         else:
-                            continue
+                             print("Verificando, por favor espere...........")
+                             print("No existe el numero de reserva a buscar en la base de datos")
+                            
             except:
-                break
+                    break
                 
         continue
-
+    print("")
     if opcion == 3:
+        costo_aleatorio=random.randint(3500,12900)
+        print("-----------------       BOLETA       -----------------")
         print("")
+        print("La fecha del viaje a cobrar, fue le día: ", acumulador_fecha)
+        print("La cantidad de mascotas a pasear fue/fueron: ", acumulador_mascotas)
+        print("-----------------------------------------------------")
+        print("El total del precio del paseo es: ", costo_aleatorio)
+        print("El total de mascotas a pasear es: ", acumulador_mascotas)
+        print("El costo por paseo es de: ",acumulador_precio)
+        print("******************************************************")
+        
+        print(f"El total a cancelar es: ")
+        usuario="Gonzalo"
+        clave=12
+        while seleccion_act:
+            seleccion_tarjeta=int(input("Como desea cancelar?\n1.TARJETA DEBITO\n2.TARJETA CREDITO\n3.EFECTIVO\n4.CANCELAR\nSeleccion una opcion:"))
+
+    #################################################################################################             TARJETA DE DEBITO         
+            
+            if seleccion_tarjeta == 1:
+                print("Deslice tarjeta de debito")
+                print("Leyendo tarjeta debito......por favor espere..")
+                ingreso_usuario=input("Ingrese nombre de usuario: ")
+                ingreso_clave=int(input("Ingrese clave de acceso: "))
+                if ingreso_usuario != usuario and ingreso_clave != 12:
+                    print("Error--- Usuario o clave de acceso no son correctos")
+                else:
+                    print("Usted ha ingresado correctamente")
+                    print(f"El monto a cancelar es: {costo_aleatorio}")
+                    opc=int(input("Por favor confirme el monto\n1.SI\n2.NO\nSeleccion: "))
+                    if opc == 1:
+                        print("Pago aceptado correctamente\nMuchas gracias por su compra\nHasta Luego")
+                        break
+                    else:
+                        print("Pago no aceptado!! - verifique e informe a cordinador")
+                        continue 
+
+################################################################################################                     TARJETA DE CREDITO
+  
+            if seleccion_tarjeta == 2:
+                print("Deslice tarjeta de credito")
+                print("Leyendo tarjeta credito......por favor espere..")
+                ingreso_usuario=input("Ingrese nombre de usuario: ")
+                ingreso_clave=int(input("Ingrese clave de acceso: "))
+                if ingreso_usuario != usuario and ingreso_clave != 12:
+                    print("Error--- Usuario o clave de acceso no son correctos")
+                else:
+                    print("Usted ha ingresado correctamente")
+                    print("")
+                    seleccion_cuota=int(input("Desea con cuotas o sin cuotas?\n1.SIN CUOTAS\n2.CON CUOTAS\nSeleccion: "))
+                    if seleccion_cuota == 1:
+                        sin_cuota=int(input("Selecciono sin cuotas\nPresiones 1.Para validar pago\n2.Para Salir\nSeleccion: "))
+                        if sin_cuota == 1:
+                            print("Pago aceptado correctamente\nMuchas gracias por su compra\nHasta Luego")
+                            break
+                        if sin_cuota == 2:
+                            print("Usted ha cancelado la operación.. Gracias")
+                            break
+                    if seleccion_cuota == 2:
+                        cuota=int(input("Ingrese cantidad de cuotas\n1. 1 CUOTA\n2. 3 CUOTAS\n3. 6 CUOTAS\nSeleccion: "))
+                        try:
+                            if cuota == 1:
+                                print(f"El valor a cancelar es {costo_aleatorio}")
+                                print("Pago aceptado correctamente\nMuchas gracias por su compra\nHasta Luego")
+                                break
+                            elif cuota == 2:
+                                print("Cada persona debe cancelar un monto de: $" , costo_aleatorio / 3)
+                                break
+                            elif cuota == 3:
+                                print("Cada persona debe cancelar un monto de: $" , costo_aleatorio / 6)
+                                break
+                            else:
+                                print("Debe seleccionar una de las 3 opciones de cuotas")
+
+                        except:
+                                print("")
+
+###########################################################################################         CANCELAR EN EFECTIVO             
+            
+            if seleccion_tarjeta == 3:
+                print("Sólo acreque a la caja y cancele el monto de $ ", costo_aleatorio)
+                print("Pago recibido por caja\nMuchas gracias por su compra\nHasta Luego")
+
+###############################################################################################    CANCELAR COMPRA
+
+            if seleccion_tarjeta == 4:
+                print("Usted ha cancelado su compra\nMuchas gracias\nHasta Luego")
+           
         continue
 
 
